313522edb5db311b5fd55ec300a4fb2eea5e2a2a
Violeta Georgieva <violetag@vmware.com>
2022-12-21 14:09:45 +0200

    Enable http/2 and wiretap

---
+++ src/main/java/com/example/content/length/MyNettyWebServerCustomizer.java
@@ -0,0 +1,14 @@
+package com.example.content.length;
+
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyNettyWebServerCustomizer implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+	@Override
+	public void customize(NettyReactiveWebServerFactory factory) {
+		factory.addServerCustomizers(httpServer -> httpServer.wiretap(true));
+	}
+}
